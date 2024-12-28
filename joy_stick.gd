7b835d6ae6f17b8e30cdb55d3f5d57d2dfb3803b
Itamars <TheWaterPaper11@gmail.com>
2024-12-28 11:53:40 +0200

    maya is looking at me

--- joy_stick.gd
+++ joy_stick.gd
@@ -1,6 +1,10 @@
 extends Node2D
+@onready var main_scene: Node2D = $".."
+@onready var window : Window = get_window()	
 
 var posVector: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	global_position = Vector2(DisplayServer.screen_get_size().x / 10, 8 * DisplayServer.screen_get_size().y / 10)
+	print(window.size)
+	print(DisplayServer.window_get_size())
+	global_position = Vector2(175, window.size.y - 175)
