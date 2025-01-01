2ab875269d442dd0d006df6cf00bf07c5d2a5069
Itamars <TheWaterPaper11@gmail.com>
2025-01-01 17:15:27 +0200

    sign fix

--- little_sign.gd
+++ little_sign.gd
@@ -1,4 +1,7 @@
 extends Node2D
+@onready var player: Player = $"../../Player"
+@onready var label: Label = $TextBoxContainer/Panel/MarginContainer/HBoxContainer/Label
+
 var popUpScene
 var popUp
 # Called when the node enters the scene tree for the first time.
@@ -6,8 +9,20 @@ func _ready() -> void:
 	popUpScene = preload("res://text_box.tscn")
 	popUp = popUpScene.instantiate()
 	add_child(popUp)
+	popUp.set_text("מאיה החמודה שלי")
 	popUp.hide_textbox()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		popUp.show_textbox()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		popUp.hide_textbox()
--- text_box.gd
+++ text_box.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 @onready var text_box_container: MarginContainer = $TextBoxContainer
+@onready var label: Label = $TextBoxContainer/Panel/MarginContainer/HBoxContainer/Label
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_text(text: String):
+	label.text = text
 
 func hide_textbox():
 	text_box_container.hide()
